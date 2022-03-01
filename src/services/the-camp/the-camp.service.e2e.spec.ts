b81dbba2d4c9c56c3702b539e7f013f16298d89a
장지훈 <kiwi.jang@toss.im>
2022-03-02 01:07:43 +0900

    test(): e2e는 테스트에서 제거

--- src/services/the-camp/the-camp.service.e2e.spec.ts
+++ src/services/the-camp/the-camp.service.e2e.spec.ts
@@ -2,7 +2,8 @@ import { theCampService } from './the-camp.service';
 
 describe.skip('TheCampService e2e', () => {
 	it('성공', async () => {
-		const session = await theCampService.login({
+		// const session = await theCampService.login({
+		await theCampService.login({
 			id: '',
 			password: '',
 		});
